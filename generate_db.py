b4995edf9d51e92205eeda7e898a32b9cc7a3fe8
ocsmit <ocsmit@protonmail.com>
2021-05-12 16:47:41 -0400

    WIP: Flexible CLI script

--- generate_db.py
+++ generate_db.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-import os
-import sys
-import re
-import sqlite3
+import argparse
+import time
 from pathlib import Path
 from connection import proj_connection
 
 # Make CLI argument
-new_database = "projpicker.db"
+PROJPICKER_DB = "projpicker.db"
+
 
 tables = {
     "projbbox": """
@@ -44,14 +43,17 @@ tables = {
             """,
 }
 
+# Open only one connection
+global proj_con
+global proj_cur
+proj_con = proj_connection()
+proj_cur = proj_con.cursor()
+
 
 def authority_codes(auth="EPSG", table="projected_crs") -> list:
     '''
     Get list of authority_codes
     '''
-    proj_con = proj_connection()
-    proj_cur = proj_con.cursor()
-
     proj_cur.execute(f"SELECT code FROM {table} WHERE auth_name = '{auth}'")
     return [code[0] for code in proj_cur.fetchall()]
 
@@ -60,9 +62,6 @@ def ancillary_codes(auth_code) -> dict:
     '''
     Get extent and scope keys from usage table
     '''
-    proj_con = proj_connection()
-    proj_cur = proj_con.cursor()
-
     proj_cur.execute(
         f"SELECT extent_code, scope_code FROM usage WHERE object_code = {auth_code}"
     )
@@ -73,9 +72,64 @@ def ancillary_codes(auth_code) -> dict:
     return codes_dict
 
 
+def code_index(auth_codes, auth="EPSG") -> dict:
+    codex = {}
+    for code in auth_codes:
+        codex[code] = ancillary_codes(code)
+    return codex
+
+
+def get_scope(code: dict, auth="EPSG") -> list:
+    scope_code = code['scope_code']
+    query = f"select scope from scope where auth_name = '{auth}' and code = {scope_code}"
+    proj_cur.execute(query)
+    return(list(proj_cur.fetchall()[0]))
+
+
+def get_extent(code: dict, auth="EPSG") -> dict:
+    extent_code = code['extent_code']
+    query = f"select name, description from extent where auth_name = '{auth}' and code = {extent_code}"
+    proj_cur.execute(query)
+    area = proj_cur.fetchall()[0]
+    extent = {'name': area[0], 'description': area[1]}
+
+    query = f"select south_lat, north_lat, west_lon, east_lon from extent where auth_name = '{auth}' and code = {extent_code}"
+    proj_cur.execute(query)
+    bbox = list(proj_cur.fetchall()[0])
+    extent['bbox'] = bbox
+    return extent
+
+
 def main():
-    pass
+    start = time.time()
+    # Create parser
+    parser = argparse.ArgumentParser(description='Generate ProjPicker sqlite database')
+    parser.add_argument('table', type=str,
+                        help='proj.db crs table to query' )
+    parser.add_argument('-l', '--location', type=str, default='./',
+                        help='Directory where to create database. Default: ./')
+    parser.add_argument('-a', '--authority', type=str, default='EPSG',
+                        help='CRS Authority')
+    # Parse arguments
+    args = parser.parse_args()
+
+    if args.table not in ['projected_crs', 'geodetic_crs', 'verticle_crs']:
+        raise Exception('Choose one of projected_crs, geodetic_crs, verticle_crs')
+
+    out_path = Path(args.location, PROJPICKER_DB)
+
+    auth_codes = authority_codes(args.authority, args.table)
+
+    usage_codes = code_index(auth_codes)
+
+    print(get_scope(usage_codes['9674']))
+    print(get_extent(usage_codes['9674']))
+
+
+
+    print(time.time() - start)
 
 
 if __name__ == "__main__":
     main()
+    proj_con.close()
