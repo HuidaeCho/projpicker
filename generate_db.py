b21e1fa70e04caf51543bb34003409954aabee55
ocsmit <ocsmit@protonmail.com>
2021-05-12 18:21:32 -0400

    Commenting

--- generate_db.py
+++ generate_db.py
@@ -4,10 +4,10 @@ import time
 from pathlib import Path
 from connection import proj_connection
 
-# Make CLI argument
+# Constant projpicker database name
 PROJPICKER_DB = "projpicker.db"
 
-
+# Tables specified on the wiki
 tables = {
     "projbbox": """
                 create table projbbox (
@@ -44,8 +44,6 @@ tables = {
 }
 
 # Open only one connection
-global proj_con
-global proj_cur
 proj_con = proj_connection()
 proj_cur = proj_con.cursor()
 
@@ -73,6 +71,9 @@ def ancillary_codes(auth_code) -> dict:
 
 
 def code_index(auth_codes, auth="EPSG") -> dict:
+    '''
+    Create full dictionary of CRS codes and their respective usage codes.
+    '''
     codex = {}
     for code in auth_codes:
         codex[code] = ancillary_codes(code)
@@ -80,6 +81,9 @@ def code_index(auth_codes, auth="EPSG") -> dict:
 
 
 def get_scope(code: dict, auth="EPSG") -> list:
+    '''
+    Retrieve scope from CRS code usage index
+    '''
     scope_code = code['scope_code']
     query = f"select scope from scope where auth_name = '{auth}' and code = {scope_code}"
     proj_cur.execute(query)
@@ -87,6 +91,9 @@ def get_scope(code: dict, auth="EPSG") -> list:
 
 
 def get_extent(code: dict, auth="EPSG") -> dict:
+    '''
+    Retrieve extent from CRS code usage index
+    '''
     extent_code = code['extent_code']
     query = f"select name, description from extent where auth_name = '{auth}' and code = {extent_code}"
     proj_cur.execute(query)
@@ -101,6 +108,9 @@ def get_extent(code: dict, auth="EPSG") -> dict:
 
 
 def get_full_usage(code: dict, auth="EPSG") -> dict:
+    '''
+    Get full usage attributes for a give CRS code
+    '''
     scope = get_scope(code, auth)
     extent = get_extent(code, auth)
     return {'scope': scope, 'area': extent}
@@ -111,30 +121,37 @@ def main():
     # Create parser
     parser = argparse.ArgumentParser(description='Generate ProjPicker sqlite database')
     parser.add_argument('table', type=str,
-                        help='proj.db crs table to query' )
+                        help='proj.db crs table to query')
     parser.add_argument('-l', '--location', type=str, default='./',
                         help='Directory where to create database. Default: ./')
     parser.add_argument('-a', '--authority', type=str, default='EPSG',
                         help='CRS Authority')
     # Parse arguments
     args = parser.parse_args()
-
+    # Check table
     if args.table not in ['projected_crs', 'geodetic_crs', 'verticle_crs']:
         raise Exception('Choose one of projected_crs, geodetic_crs, verticle_crs')
-
+    # Output table path
     out_path = Path(args.location, PROJPICKER_DB)
 
+    # Full list of CRS codes in the specified table
     auth_codes = authority_codes(args.authority, args.table)
 
+    # Usage index codes for CRS
     usage_codes = code_index(auth_codes)
 
+    # Create full usage dictionary with scope and extent for each CRS code
     usage_dict = {}
     for code in usage_codes:
         usage_dict[code] = get_full_usage(usage_codes[str(code)])
 
     print(time.time() - start)
 
+    # temporary return
+    return usage_dict
+
 
 if __name__ == "__main__":
     main()
+    # Close connection
     proj_con.close()
