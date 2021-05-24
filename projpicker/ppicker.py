803f93840e049b676371dac8b8f127b49cfb123a
ocsmit <ocsmit@protonmail.com>
2021-05-24 18:34:12 -0400

    Add headers

--- projpicker/ppicker.py
+++ projpicker/ppicker.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+################################################################################
+# Project: ProjPicker
+# Purpose: This Python script provides the main CLI API for ProjPicker.
+# Author(s): Huidae Cho, Owen Smith
+# Since:   June 4, 2021
+#
+# Copyright (C) 2021, Huidae Cho <https://faculty.ung.edu/hcho/>,
+#                     Owen Smith <https://www.gaderian.io/>
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 
 import pprint
 import argparse
