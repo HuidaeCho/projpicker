f4eb1b2632d1b9c0438bee0f1f659f56d008e891
ocsmit <ocsmit@protonmail.com>
2021-05-25 17:13:27 -0400

    Update constant file paths to ensure their constant location for both creation and reading

--- projpicker/utils/const.py
+++ projpicker/utils/const.py
@@ -1,5 +1,15 @@
 # CONSTANTS
+from pathlib import Path
 
-PROJPICKER_DB = "projpicker.db"
-RTREE = "projpicker_rtree"
+# Get file path for const.py
+file = Path(__file__)
+
+# Create file path within package directory for data
+data_dir = file.parent.parent / 'data'
+if not data_dir.exists():
+    Path.mkdir(data_dir)
+
+# Ancillary data will be created and read from within the package directory
+PROJPICKER_DB = str(data_dir / 'projpicker.db')
+RTREE = str(data_dir / "projpicker_rtree")
 
