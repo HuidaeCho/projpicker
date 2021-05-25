360d8519f87e12b5d3ea1cc1938f73ee8c987b3b
ocsmit <ocsmit@protonmail.com>
2021-05-25 16:20:26 -0400

    Add __all__ for * imports

--- projpicker/__init__.py
+++ projpicker/__init__.py
@@ -1,3 +1,5 @@
 from .utils.geom import get_geom, get_bounds, intersect
 from .utils.connection import projpicker_connection
 
+__all__ = ["get_geom", "get_bounds", "intersect", "projpicker_connection"]
+
