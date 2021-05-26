3c2e05320a7500339b9000bdb47bfd62089f373d
ocsmit <ocsmit@protonmail.com>
2021-05-26 11:55:29 -0400

    Proj connection class

--- projpicker/core/connection.py
+++ projpicker/core/connection.py
@@ -1,6 +1,5 @@
 import sys
 import sqlite3
-from dataclasses import dataclass
 from distutils.spawn import find_executable
 from pathlib import Path
 from .const import PROJPICKER_DB
@@ -12,28 +11,37 @@ def _validate_proj(data_dir):
     return False
 
 
-def proj_connection():
-    try:
-        # Returns '/usr/bin/proj' on native Linux install.
-        # Resource files, i.e. proj.db, are installed at '/usr/share/proj'
-        find_executable("proj")
-    except:
-        raise Exception("No PROJ installation detected")
+class ProjConnection():
+    def __init__(self):
+        try:
+            # Returns '/usr/bin/proj' on native Linux install.
+            # Resource files, i.e. proj.db, are installed at '/usr/share/proj'
+            find_executable("proj")
+        except:
+            raise Exception("No PROJ installation detected")
 
-    # Need dynamic solution to account for different OS's,
-    # proj installations, and potential conda installs.
-    # See https://github.com/pyproj4/pyproj/pyproj/datadir.py for
-    # example implementation.
+        # Need dynamic solution to account for different OS's,
+        # proj installations, and potential conda installs.
+        # See https://github.com/pyproj4/pyproj/pyproj/datadir.py for
+        # example implementation.
 
-    # However, dynamic solution would only be needed if we plan on making our
-    # connection reproducible.
-    resource_path = Path(sys.prefix, "share", "proj")
-    if _validate_proj(resource_path):
-        proj_db = Path(resource_path, "proj.db")
-    else:
-        raise Exception("No proj.db")
+        # However, dynamic solution would only be needed if we plan on making our
+        # connection reproducible.
+        resource_path = Path(sys.prefix, "share", "proj")
+        if _validate_proj(resource_path):
+            proj_db = Path(resource_path, "proj.db")
+        else:
+            raise Exception("No proj.db")
 
-    return sqlite3.connect(proj_db)
+        self.__db = sqlite3.connect(proj_db)
+        self.cur = self.__db.cursor()
+
+    def close(self):
+        self.__db.close()
+
+    def query(self, sql):
+        self.cur.execute(sql)
+        return self.cur.fetchall()
 
 
 def projpicker_connection():
