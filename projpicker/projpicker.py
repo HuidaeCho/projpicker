085c0350b28c03723aa37316cca21a33befcf5dd
Huidae Cho <grass4u@gmail.com>
2021-06-05 16:16:58 -0400

    Remove collections import (used only in common.py)

--- projpicker/projpicker.py
+++ projpicker/projpicker.py
@@ -25,8 +25,6 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ################################################################################
 
-# no third-party modules!
-import collections
 import argparse
 import os
 import sys
