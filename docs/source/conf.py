51d7d7ce94e9d445bc180c5289c29a384a19660f
ocsmit <ocsmit@protonmail.com>
2021-06-04 21:36:06 -0400

    Add link to github in top right corner

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,6 +52,12 @@ exclude_patterns = []
 #
 html_theme = "press"
 
+html_theme_options = {
+  "external_links": [
+      ("Github", "https://github.com/HuidaeCho/projpicker"),
+  ]
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
