1a7a13f9a55042facf467472ed2b6e8e15ac327d
ocsmit <ocsmit@protonmail.com>
2021-06-16 16:37:22 -0400

    Change theme to sphinx_rtd_docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
+#sphinx_rtd_theme Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
@@ -52,7 +52,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
