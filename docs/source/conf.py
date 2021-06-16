ad53b29a1ae13567ee53c7101eadcb712b4ee211
ocsmit <ocsmit@protonmail.com>
2021-06-16 15:58:38 -0400

    Revert to default theme to test toctree external link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,13 +51,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "press"
+#html_theme = "press"
 
-html_theme_options = {
-  "external_links": [
-      ("GitHub", "https://github.com/HuidaeCho/projpicker"),
-  ]
-}
+#html_theme_options = {
+#  "external_links": [
+#      ("GitHub", "https://github.com/HuidaeCho/projpicker"),
+#  ]
+#}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
