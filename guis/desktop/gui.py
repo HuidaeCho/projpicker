1dad8e0900ac7238f723cb8050ed80136812903b
ocsmit <ocsmit@protonmail.com>
2021-06-30 18:46:17 -0400

    Shebang, header

--- guis/desktop/gui.py
+++ guis/desktop/gui.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python3
+################################################################################
+# Project:  ProjPicker (Projection Picker) Desktop GUI
+#           <https://github.com/HuidaeCho/projpicker>
+# Authors:  Owen Smith, Huidae Cho
+#           Institute for Environmental and Spatial Analysis
+#           University of North Georgia
+# Since:    May 27, 2021
+#
+# Copyright (C) 2021 Huidae Cho <https://faculty.ung.edu/hcho/> and
+#                    Owen Smith <https://www.gaderian.io/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+################################################################################
 import json
 import textwrap
 import pprint
