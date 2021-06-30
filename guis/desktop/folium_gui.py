233b751906b23dc0eeb24bca32eb1eeb7b94d6f8
ocsmit <ocsmit@protonmail.com>
2021-06-29 20:55:30 -0400

    Black formatting

--- guis/desktop/folium_gui.py
+++ guis/desktop/folium_gui.py
@@ -21,7 +21,8 @@ from folium.plugins import Draw
 from pathlib import Path
 import projpicker as ppik
 
-MAP = 'openstreet.html'
+MAP = "openstreet.html"
+
 
 def generate_map():
     map_path = Path(MAP)
@@ -33,12 +34,13 @@ def generate_map():
         fmap = folium.Map([34.2347566, -83.8676613], zoom_start=5)
         Draw(
             export=False,
-            filename='my_data.geojson',
-            position='topleft',
-            draw_options={'polyline': {'allowIntersection': False}},
-            edit_options={'poly': {'allowIntersection': False}}
+            filename="my_data.geojson",
+            position="topleft",
+            draw_options={"polyline": {"allowIntersection": False}},
+            edit_options={"poly": {"allowIntersection": False}},
         ).add_to(fmap)
         fmap.save(MAP)
+
 
 class MainFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
@@ -52,24 +54,25 @@ class MainFrame(wx.Frame):
 
         #################################
         # LEFT
-        st = wx.StaticText(panel, -1, "CRS List", pos=(0,0))
+        st = wx.StaticText(panel, -1, "CRS List", pos=(0, 0))
         left.Add(st, 0, wx.CENTER | wx.TOP | wx.BOTTOM, 10)
 
         # CRS Choice listbox
-        lbox = wx.ListBox(panel,
-                id=1,
-                size=(300, 600),
-                choices=['test1', 'test2'],
-                )
+        lbox = wx.ListBox(
+            panel,
+            id=1,
+            size=(300, 600),
+            choices=["test1", "test2"],
+        )
 
         # Add CRS listbox to main left side
-        left.Add(lbox, 1, wx.ALIGN_RIGHT| wx.ALL | wx.BOTTOM, 0)
+        left.Add(lbox, 1, wx.ALIGN_RIGHT | wx.ALL | wx.BOTTOM, 0)
         # Create bottom left sizer for buttons
         btm_left = wx.BoxSizer(wx.HORIZONTAL)
         # Ok button
-        btn_ok = wx.Button(panel, label='Ok')
+        btn_ok = wx.Button(panel, label="Ok")
         # Cancel button
-        btn_cancel = wx.Button(panel, label='Cancel')
+        btn_cancel = wx.Button(panel, label="Cancel")
         # Add buttons to bottom left
         btm_left.Add(btn_ok, 30, wx.LEFT | wx.RIGHT, 30)
         btm_left.Add(btn_cancel, 30, wx.LEFT | wx.RIGHT, 30)
@@ -103,7 +106,6 @@ class MainFrame(wx.Frame):
         def confirm_load(event):
             print("OpenStreetMap loaded.")
 
-
         Url = wx.FileSystem.FileNameToURL(MAP)
         self.browser.LoadURL(Url)
         wx.EvtHandler.Bind(self, wx.html2.EVT_WEBVIEW_LOADED, confirm_load)
@@ -134,33 +136,31 @@ class MainFrame(wx.Frame):
         self.query()
 
     def __reverse_lat_lon(self, coors):
-        '''
+        """
         Switch lat lon
-        '''
+        """
         corrected_coors = []
         for i in coors[0]:
             corrected_coors.append(i[::-1])
         return corrected_coors
 
     def query(self):
-        features = self.json['features']
+        features = self.json["features"]
         if len(features) == 1:
-            coors = features[0]['geometry']['coordinates']
+            coors = features[0]["geometry"]["coordinates"]
             pprint.pprint(coors)
             corrected = self.__reverse_lat_lon(coors)
             bbox = ppik.query_polys(corrected)
             # Print bbox query for now
             ppik.print_bbox(bbox)
         else:
-            print('Handle multiple geometry')
-
+            print("Handle multiple geometry")
 
         # TODO: Modularize the code to enable event handlers based off the EVT_WEBVIEW_TITLE_CHANGED
         #       event.
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = wx.App()
     frame = MainFrame(None)
     frame.Show()
