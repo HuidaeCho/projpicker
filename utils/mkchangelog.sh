ac5767a685151872ef7278bc6274bcb693f99bf3
Huidae Cho <grass4u@gmail.com>
2021-07-08 10:47:39 -0400

    mkchangelog.sh: HEAD not always available

--- utils/mkchangelog.sh
+++ utils/mkchangelog.sh
@@ -13,10 +13,11 @@ for i in COPYING README.md setup.py data deploy docs guis tests utils; do
 done
 (
 echo "# Change log"
+echo
 git log --pretty="* $hash %d %s %cd" --decorate=full $exclude |
 sed '
 /(HEAD/{
-	s/^\(.*\) (HEAD.*HEAD) \(.* \(\([^ ]\+ \)\{5\}[^ ]\+\)\)$/\n## HEAD\n\3\n\n\1 \2/
+	s/^\(.*\) (HEAD.*HEAD) \(.* \(\([^ ]\+ \)\{5\}[^ ]\+\)\)$/## HEAD\n\3\n\n\1 \2/
 }
 /(tag: refs\/tags/{
 	s/^\(.*(tag: refs\/tags\/\([^)]*\)).* \(\([^ ]\+ \)\{5\}[^ ]\+\)\)$/\n## \2\n\3\n\n\1/
