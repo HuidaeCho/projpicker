1deafd8e1f0896f118bca9b2f0e74b607fb4e8de
Huidae Cho <grass4u@gmail.com>
2021-07-08 10:44:24 -0400

    mkchangelog.sh: Support stdout

--- utils/mkchangelog.sh
+++ utils/mkchangelog.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
+if [ "$1" = "-w" ]; then
+	file=../projpicker/ChangeLog.md
+	hash="[%h](https://github.com/HuidaeCho/projpicker/commit/%h)"
+else
+	file=/dev/stdout
+	hash="%h"
+fi
+
 exclude=""
 for i in COPYING README.md setup.py data deploy docs guis tests utils; do
 	exclude="$exclude :(exclude)../$i"
 done
 (
 echo "# Change log"
-git log --pretty="* [%h](https://github.com/HuidaeCho/projpicker/commit/%h) %d %s %cd" --decorate=full $exclude |
+git log --pretty="* $hash %d %s %cd" --decorate=full $exclude |
 sed '
 /(HEAD/{
 	s/^\(.*\) (HEAD.*HEAD) \(.* \(\([^ ]\+ \)\{5\}[^ ]\+\)\)$/\n## HEAD\n\3\n\n\1 \2/
@@ -16,4 +24,4 @@ sed '
 s/\( [^ ]\+\)\{6\}$//
 s/_/\\_/g
 '
-) > ../projpicker/ChangeLog.md
+) > $file
