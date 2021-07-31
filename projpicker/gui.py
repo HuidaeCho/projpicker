fdd53a61f01901ce3e14be3f12a9a7717efffe5b
Huidae Cho <grass4u@gmail.com>
2021-07-30 20:14:27 -0400

    Add PROJPICKER_GUI env var to select wx or tk GUI

--- projpicker/gui.py
+++ projpicker/gui.py
@@ -1,14 +1,27 @@
 """
-TODO
+This module imports the gui_wx or gui_tk module.
 """
+
+import os
+
+projpicker_gui_env = "PROJPICKER_GUI"
+
+gui = os.environ.get(projpicker_gui_env, "wx")
+
 # https://stackoverflow.com/a/49480246/16079666
 if __package__:
-    try:
-        from .gui_wx import *
-    except:
+    if gui == "tk":
         from .gui_tk import *
+    else:
+        try:
+            from .gui_wx import *
+        except:
+            from .gui_tk import *
 else:
-    try:
-        from gui_wx import *
-    except:
+    if gui == "tk":
         from gui_tk import *
+    else:
+        try:
+            from gui_wx import *
+        except:
+            from gui_tk import *
