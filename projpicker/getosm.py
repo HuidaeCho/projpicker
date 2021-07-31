584b265395cd9fef43cfd9600deedaa0d2893cfb
Huidae Cho <grass4u@gmail.com>
2021-07-31 03:41:31 -0400

    OSM *tile* downloader

--- projpicker/getosm.py
+++ projpicker/getosm.py
@@ -19,7 +19,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ###############################################################################
 """
-This module provides an OpenStreetMap downloader.
+This module provides an OpenStreetMap tile downloader.
 """
 
 import sys
