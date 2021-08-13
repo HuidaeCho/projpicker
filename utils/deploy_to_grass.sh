fc9afcf6b6eec9c93582bf8be703aa0dc4b1ac9e
Huidae Cho <grass4u@gmail.com>
2021-08-13 15:23:03 -0400

    Update deploy_to_grass.sh (BE careful not to overwrite i18n messages later)

--- utils/deploy_to_grass.sh
+++ utils/deploy_to_grass.sh
@@ -4,43 +4,67 @@ set -e
 GRASS_DIR=~/usr/grass/grass
 CORE_DIR=$GRASS_DIR/python/grass/projpicker
 GUI_DIR=$GRASS_DIR/gui/wxpython/projpicker_gui
+GETOSM_DIR=$GRASS_DIR/python/grass/projpicker
 
 cd ../projpicker
-cp VERSION __init__.py common.py coor_*.py projpicker.db $CORE_DIR
+cp VERSION __init__.py common.py projpicker.db $CORE_DIR
+
+for i in coor_latlon.py coor_xy.py; do
+sed '
+/^if __package__:$/,/^else:$/{s/^    //}
+/^if __package__:$/d
+/^else:$/i\
+
+/^else:$/,/^$/d
+' $i > $CORE_DIR/$i
+done
 
 sed '
 /^has_web = True$/a\
-\
-try:\
-    from grass.script.setup import set_gui_path\
-    set_gui_path()\
-    from projpicker_gui import gui\
-except:\
-    hsa_gui = False
-/^    from . import coor_xy$/i\
-    from . import coor_xy
-/^    from . import coor_xy$/,+4d
-/^    import coor_xy$/i\
-    import coor_xy
-/^    import coor_xy$/,+4d
-' projpicker.py > $CORE_DIR/projpicker.py
+from grass.script.setup import set_gui_path\
+set_gui_path()\
+from projpicker_gui import gui
+/^if __package__:$/,/^else:$/{s/^    //}
+/^if __package__:$/d
+/^else:$/i\
 
-cp getosm.py $GUI_DIR
+/^else:$/,/^$/d
+/^try:$/,/^    has/d
+/^has/d
+/^# https:\/\/stackoverflow/d
+/if not has_gui:$/,/^$/d
+/^# command-line interface$/,/^##########/d
+/^set_latlon()$/i\
+set_latlon()
+/^set_latlon()$/,$d
+' projpicker.py > $CORE_DIR/projpicker.py
 
 sed '
 /^if __package__:$/i\
-import grass.projpicker as ppik
-/^if __package__:$/,+3d
+import grass.projpicker as ppik\
+
+/^if __package__:$/,/^$/d
 ' gui_common.py > $GUI_DIR/gui_common.py
 
 sed '
 /^if __package__:$/i\
-import grass.projpicker as ppik
-/import projpicker as ppik/d
+import grass.projpicker as ppik\
+from grass.getosm import OpenStreetMap\
+
+/^if __package__:$/,/^else:$/{s/^    //}
+/^if __package__:$/d
+/^else:$/i\
+
+/^else:$/,/^$/d
+/import \(projpicker\|OpenStreetMap\)/d
 ' gui_wx.py > $GUI_DIR/gui.py
 
-sed '
-/^if __package__:$/i\
-import grass.projpicker as ppik
-/import projpicker as ppik/d
-' wxwidgets.py > $GUI_DIR/wxwidgets.py
+# $GUI_DIR/panel.py, derived from wxwidgets.py, is highly modified for GRASS
+# GIS; do not copy it anymore
+#sed '
+#/^if __package__:$/i\
+#import grass.projpicker as ppik
+#/import projpicker as ppik/d
+#' wxwidgets.py > $GUI_DIR/wxwidgets.py
+
+cp getosm.py $GETOSM_DIR
